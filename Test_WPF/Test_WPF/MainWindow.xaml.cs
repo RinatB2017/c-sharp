fffb27abd354dd305ce47d4f5965c26ac95d4609
RinatB2017 <tux4096@gmail.com>
2018-11-02 22:17:59 +0300

    update

--- Test_WPF/Test_WPF/MainWindow.xaml.cs
+++ Test_WPF/Test_WPF/MainWindow.xaml.cs
@@ -24,5 +24,12 @@ namespace Test_WPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Текст",
+                "Сообщение");
+        }
     }
 }
