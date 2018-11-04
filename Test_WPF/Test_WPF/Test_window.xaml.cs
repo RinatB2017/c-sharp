fa75079d4e733d8130c6bc18e1e53359ef678132
RinatB2017 <tux4096@gmail.com>
2018-11-04 22:44:17 +0300

    update

--- Test_WPF/Test_WPF/Test_window.xaml.cs
+++ Test_WPF/Test_WPF/Test_window.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace Test_WPF
 {
     /// <summary>
@@ -22,6 +23,37 @@ namespace Test_WPF
         public Test_window()
         {
             InitializeComponent();
+
+            test();
+        }
+
+        public void test()
+        {
+            // Create a StackPanel to contain the shape.
+            StackPanel myStackPanel = new StackPanel();
+
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the 
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values. 
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = 200;
+            myEllipse.Height = 200;
+
+            // Add the Ellipse to the StackPanel.
+            myStackPanel.Children.Add(myEllipse);
+
+            this.Content = myStackPanel;
         }
     }
 }
